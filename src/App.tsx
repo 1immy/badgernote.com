936123dd37250b0e9d3dceb4d48468bd1c119725
Limo Kemei <ohlimmy@proton.me>
2025-03-13 19:27:03 +0300

    [misc] Code cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import UploadPage from "./pages/UploadPage";
 import BrowsePage from "./pages/BrowsePage";
@@ -8,14 +8,14 @@ import "./styles.css";
 
 const App: React.FC = () => {
   return (
-    <Router>
+    <BrowserRouter>
       <Navbar />
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/upload" element={<UploadPage />} />
         <Route path="/browse" element={<BrowsePage />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 };
 
